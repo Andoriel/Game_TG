748641c9a7134d00a7d7ebacf0e803805410f965
Andoriel <andoriel@live.com>
2023-11-14 18:36:50 -0300

    menuInicialparte2

---
+++ Assets/Scripts/MenuMainManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMainManager : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    [SerializeField] private GameObject panelMainMenu;
+    [SerializeField] private GameObject panelCredits;
+
+    /*
+     * public void Play()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+    */
+
+
+    public void OpenCredits()
+    {
+        panelCredits.SetActive(true);
+        panelMainMenu.SetActive(false);
+    }
+
+    public void CloseCredits()
+    {
+        panelCredits.SetActive(false);
+        panelMainMenu.SetActive(true);
+    }
+
+
+    public void ExitGame()
+    {
+        Debug.Log("Saiu do Jogo");
+        //Application.Quit();
+    }
+}
